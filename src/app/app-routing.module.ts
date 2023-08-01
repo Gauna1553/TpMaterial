97b02ac2fe9146d769cd15af476797142b5a7fe2
Santino0912 <ssantinomunnoz@gmail.com>
2023-08-01 09:32:29 -0300

    Configure las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+  path: 'inicio',
+  component:InicioComponent
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/nosotros/nosotros.module').then(m=>m.NosotrosModule)
+  },
+  {
+    path:"",loadChildren:()=>import('./modules/shared/shared.module').then(m=>m.SharedModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
