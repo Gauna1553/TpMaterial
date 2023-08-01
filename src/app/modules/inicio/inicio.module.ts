282d614863b7a2ca9f18443fdc01aafefb6a685c
Santino0912 <ssantinomunnoz@gmail.com>
2023-08-01 10:25:30 -0300

    cree el componente nosotros y configure mas rutas

--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InicioComponent } from './pages/inicio/inicio.component';
+import { Routes, RouterModule } from '@angular/router';
 
+const routes: Routes=[
+  {
+    path:'inicio',component:InicioComponent
+  }
 
+]
 
 @NgModule({
   declarations: [
     InicioComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    [RouterModule.forChild(routes)],
+    [RouterModule]
   ]
 })
 export class InicioModule { }
--- src/app/modules/nosotros/nosotros.module.ts
+++ src/app/modules/nosotros/nosotros.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { NosotrosComponent } from './nosotros.component';
 
+
+const routes: Routes = [
+  {
+    path:'nosotros',component:NosotrosComponent
+  }
+]
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NosotrosComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    [RouterModule.forChild(routes)],
+    [RouterModule]
   ]
 })
 export class NosotrosModule { }
